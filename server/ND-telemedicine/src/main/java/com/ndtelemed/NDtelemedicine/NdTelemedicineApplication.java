53b7decdb4dd837a22bc7f45950cc9644a389740
Angelo Carasig <s3906344@student.rmit.edu.au>
2022-08-04 18:28:16 +1000

    initialized front and backend flutter/springboot

---
+++ server/ND-telemedicine/src/main/java/com/ndtelemed/NDtelemedicine/NdTelemedicineApplication.java
@@ -0,0 +1,12 @@
+package com.ndtelemed.NDtelemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdTelemedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdTelemedicineApplication.class, args);
+	}
+}
---
+++ server/ND-telemedicine/src/test/java/com/ndtelemed/NDtelemedicine/NdTelemedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndtelemed.NDtelemedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdTelemedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
