412bad599512f37cc68a97ab281f2439dbfb3cac
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-04 21:29:48 +1000

    connected front end to data

--- server/ND-Telemedecine/src/main/java/com/example/ndtelemedecine/UserRepo.java
+++ server/ND-Telemedecine/src/main/java/com/example/ndtelemedecine/UserRepo.java
@@ -3,5 +3,5 @@ package com.example.ndtelemedecine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
-    
+    User findByEmailAndPasswordAndRole(String email,String password, String role);
 }
