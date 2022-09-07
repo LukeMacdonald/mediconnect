ccb5ec899fcc90fd56a66e459d1a1f018f2be3ef
Jeffrey Siaw <s3902121@student.rmit.edu.au>
2022-09-07 22:10:59 +1000

    Design setting availability page

---
+++ client/lib/set_availability.dart
@@ -0,0 +1,169 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+
+import 'user.dart';
+
+//TODO: https://stackoverflow.com/questions/59425633/flutter-create-dynamic-textfield-when-button-click
+class SetAvailability extends StatefulWidget {
+  const SetAvailability({Key? key}) : super(key: key);
+
+  @override
+  State<SetAvailability> createState() => _SetAvailability();
+}
+
+class _SetAvailability extends State<SetAvailability> {
+  User user = User("", "", "");
+  // TextEditingController dateInput = TextEditingController();
+  var dayTECs = <int, TextEditingController>{};
+  var hourTECs = <int, TextEditingController>{};
+  List<Entry> entries = [];
+  String url = "http://localhost:8080/set_availability";
+
+  // Change this for connection to backend
+  Future save() async {
+    await http.post(Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'email': user.email,
+          'password': user.password,
+          'role': user.role
+        }));
+  }
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Widget availabilityList() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(height: 10),
+          Container(
+              constraints: const BoxConstraints(
+                  minWidth: 800, maxWidth: 800, minHeight: 300, maxHeight: 300),
+              decoration: BoxDecoration(
+                  color: const Color(0x86C6F7FD),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 6,
+                        offset: Offset(0, 2))
+                  ]),
+              height: 60,
+              // TODO: add a list of the set availibility (list_view)
+              child: Text(
+                  '    List of Each Day of the Week and the option to select what hours the doctor is available to work',
+                  style: GoogleFonts.lexendDeca(
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  )))
+        ]);
+  }
+
+  Widget setting() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[const SizedBox(height: 10), Container()]);
+  }
+
+  Widget availability() {
+    return SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Column(children: [
+          const Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 0)),
+          availabilityList(),
+          Align(
+              alignment: const AlignmentDirectional(-0.57, 0.05),
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                child: Text('Specific Dates Not Available',
+                    style: GoogleFonts.lexendDeca(
+                        textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ))),
+              )),
+          Container(
+              constraints: const BoxConstraints(minWidth: 800, maxWidth: 800),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 6,
+                        offset: Offset(0, 2))
+                  ]),
+              height: 200,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(380, 0, 0, 0),
+                    child: Container(
+                      constraints:
+                          const BoxConstraints(minWidth: 200, maxWidth: 200),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Icon(Icons.calendar_today_rounded,
+                          color: Colors.black, size: 200),
+                    ),
+                  ),
+                ],
+              ))
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: scaffoldKey,
+        body: Container(
+            height: MediaQuery.of(context).size.height * 1,
+            decoration: BoxDecoration(
+                color: const Color(0xFF14181B),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: const AssetImage('images/background.jpeg'),
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), BlendMode.darken))),
+            child: SingleChildScrollView(
+                child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    width: double.infinity,
+                    height: 20,
+                    decoration: const BoxDecoration(color: Colors.transparent)),
+                Text('Set Availability',
+                    style: GoogleFonts.roboto(
+                      textStyle:
+                          const TextStyle(color: Colors.white, fontSize: 60),
+                    )),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                  child: Container(
+                      //height: MediaQuery.of(context).size.height * 1,
+                      constraints:
+                          const BoxConstraints(minWidth: 700, minHeight: 580),
+                      decoration: const BoxDecoration(color: Color(0x00FFFFFF)),
+                      child: availability()),
+                ),
+              ],
+            ))));
+  }
+}
+
+class Entry {
+  final String? day;
+  final String? hour;
+  Entry(this.day, this.hour);
+}
