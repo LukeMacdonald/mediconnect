731776e0947bd3e9c6730f4048ca150f68992e95
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2022-09-17 16:17:21 +1000

    added set doctor availbility test

---
+++ server/ND-Telemedecine/src/test/java/com/example/ndtelemedecine/DoctorAvailabilityTest.java
@@ -0,0 +1,159 @@
+package com.example.ndtelemedecine;
+
+import com.example.ndtelemedecine.Controllers.UserApiController;
+import com.example.ndtelemedecine.Controllers.AvailabilityApiController;
+import com.example.ndtelemedecine.Models.User;
+import com.example.ndtelemedecine.Models.Availability;
+
+import com.example.ndtelemedecine.Repositories.AvailabilityRepo;
+import com.example.ndtelemedecine.Repositories.UserRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DoctorAvailabilityTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SpyBean
+    private UserApiController mockUserApiController;
+    
+    @SpyBean
+    private AvailabilityApiController mockApiController;
+
+    @MockBean
+    private static UserRepo mockUserRepo;
+
+    @MockBean
+    private AvailabilityRepo mostAvailabilityRepo;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    private static User mockDoctor;
+
+    private Availability avail;
+
+    @BeforeAll
+    static void setDoctor(){
+        mockDoctor = new User();
+        mockDoctor.setEmail("doctor@unittest.com");
+        mockDoctor.setPassword("password");
+        mockDoctor.setRole("doctor"); 
+    }
+    @BeforeEach
+    void mockData(){
+        Mockito.when(mockUserRepo.findById(0)).thenReturn(mockDoctor);
+        avail = new Availability();
+        avail.setdoctor_id(mockDoctor.getID());
+    }
+    @Test
+    void setDoctorAvailability_ReturnsTrue() throws Exception{
+        
+        avail.setstart_time("12:00");
+        avail.setend_time(":13:00");
+        avail.setday_of_week(1);
+    
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(avail);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/SetDoctorAvailability")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Availability Set!"));
+    }
+
+    @Test
+    void setDoctorAvailability_BoundryHighFalse() throws Exception{
+
+        avail.setstart_time("09:00:00");
+        avail.setend_time("10:00:00");
+        avail.setday_of_week(7);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(avail);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/SetDoctorAvailability")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Incorrect Entry: Day of Week Entered was To High"));
+    }
+    @Test
+    void setDoctorAvailability_BoundryHighReturnsTrue() throws Exception{
+        
+        avail.setstart_time("09:00:00");
+        avail.setend_time("10:00:00");
+        avail.setday_of_week(6);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(avail);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/SetDoctorAvailability")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Availability Set!"));
+    }
+    @Test
+    void setDoctorAvailability_BoundryLowFalse() throws Exception{
+
+        avail.setstart_time("09:00:00");
+        avail.setend_time("10:00:00");
+        avail.setday_of_week(0);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(avail);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/SetDoctorAvailability")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Incorrect Entry: Day of Week Entered was To Low"));
+    }
+    @Test
+    void setDoctorAvailability_BoundryLowReturnsTrue() throws Exception{
+        
+        avail.setstart_time("09:00:00");
+        avail.setend_time("10:00:00");
+        avail.setday_of_week(1);
+    
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        String requestJson = ow.writeValueAsString(avail);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/SetDoctorAvailability")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Availability Set!"));
+    }
+}
+
