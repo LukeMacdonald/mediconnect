c2008cb24b420970895e1ffbd5698599032c34a4
Luke <lukemacdonald21@gmail.com>
2022-09-29 21:01:49 +1000

    updated helper object

--- client/lib/widgets/helpers.dart
+++ client/lib/widgets/helpers.dart
@@ -3,7 +3,7 @@ abstract class Helpers {
   static final random = Random();
 
   static String randomPictureUrl() {
-    final randomInt = random.nextInt(1000);
+    final randomInt = random.nextInt(1);
     return 'https://picsum.photos/seed/$randomInt/300/300';
   }
 }
