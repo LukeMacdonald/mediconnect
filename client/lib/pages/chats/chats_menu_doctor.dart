947948bb9988073e3d8764735823d29384d0cfe7
Luke <lukemacdonald21@gmail.com>
2022-10-03 21:58:10 +1100

    added chat menu for doctor

---
+++ client/lib/pages/chats/chats_menu_doctor.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../../pages/imports.dart';
+import '../../utilities/custom_functions.dart';
+
+class ChatMenuDoctor extends StatefulWidget {
+  const ChatMenuDoctor({Key? key}) : super(key: key);
+
+  @override
+  State<ChatMenuDoctor> createState() => _ChatMenuDoctor();
+}
+
+class _ChatMenuDoctor extends State<ChatMenuDoctor> {
+  late List<MessageData> _messages;
+  late List<String> _names;
+  final int pageIndex = 1;
+
+  Future<void> getMessages() async {
+    String id = "";
+    await UserSecureStorage.getID().then((value) => id = value!);
+    var response = await http.get(
+        Uri.parse("${messageIP}get/message_menu/${int.parse(id)}"),
+        headers: {'Content-Type': 'application/json'});
+    var responses = json.decode(response.body) as List;
+    MessageData message;
+    for (var element in responses) {
+      message = MessageData(
+          element['messageID'],
+          element['senderID'],
+          element['receiverID'],
+          DateTime.parse(element['timestamp']),
+          element['message'],
+          element['viewed'] as bool);
+      String name = await getName(message.receiverID);
+      setState(() {
+        _messages.add(message);
+        _names.add(name);
+      });
+    }
+  }
+
+
+
+  @override
+  void initState() {
+    _messages = [];
+    _names = [];
+    getMessages();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: AppBarItem(
+            icon: CupertinoIcons.home,
+            index: pageIndex,
+          ),
+          title: const Text("Messages",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              )),
+          actions: const <Widget>[
+            AppBarItem(
+              icon: CupertinoIcons.bell_fill,
+              index: 5,
+            ),
+            AppDropDown(),
+          ],
+        ),
+        body: Column(children: [
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListView.builder(
+                    itemCount: _messages.length,
+                    itemBuilder: (context, index) {
+                      return ChatTile(
+                        messageData: _messages[index],
+                        name: _names[index],
+                      );
+                    },
+                  ))),
+        ]),
+        bottomNavigationBar: DoctorBottomNavigationBar(pageIndex: pageIndex));
+  }
+}
---
+++ client/lib/pages/chats/new_chat.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../../pages/imports.dart';
+
+class NewChat extends StatefulWidget {
+  const NewChat({Key? key}) : super(key: key);
+
+  @override
+  _NewChatState createState() => _NewChatState();
+}
+
+class _NewChatState extends State<NewChat> {
+
+  late List<MessageData> _messages;
+  late List<String> _names;
+  late List<dynamic> _doctors;
+
+  @override
+  void initState() {
+    _messages = [];
+    _names = [];
+    _doctors = [];
+    getDoctors();
+    super.initState();
+  }
+
+  Future<void> getDoctors()async{
+    String role = "";
+    await UserSecureStorage.getRole().then((value) => role = value!);
+    if(role == "patient") {
+      String id = "";
+      await UserSecureStorage.getID().then((value) => id = value!);
+      var response = await http.get(
+          Uri.parse("${appointmentIP}search/appointment_doctors/${int.parse(id)}"),
+          headers: {'Content-Type': 'application/json'});
+
+      _doctors = json.decode(response.body) as List;
+
+      for(int i = 0;i < _doctors.length;i++) {
+        _messages.add(MessageData(null, int.parse(id), _doctors[i], DateTime.now(), "", false));
+        response = await http.get(
+            Uri.parse("${authenticationIP}get/name/${_doctors[i]}"),
+            headers: {'Content-Type': 'application/json'});
+        _names.add(response.body);
+      }
+      setState(() {
+
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            iconTheme: Theme.of(context).iconTheme,
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leadingWidth: 54,
+            leading: Align(
+              alignment: Alignment.centerRight,
+              child: IconBackground(
+                icon: CupertinoIcons.back,
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+            title: const Text("New Message"),
+            ),
+        body:Column(mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+         Padding(
+           padding: const EdgeInsets.all(20.0),
+           child: Text("Available Doctors to Message",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).textTheme.bodyMedium?.color),
+            ),
+         ),
+          Expanded(
+              child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return ChatTile(
+                    messageData: _messages[index],
+                    name: _names[index],
+                  );
+                },
+              ))),
+    ]));
+  }
+}
