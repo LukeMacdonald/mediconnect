05aa6d95e77fa163c60280a09fae8a26e66c2693
angelo <angelocarasig15@gmail.com>
2022-10-17 00:14:46 +1100

    Attempt fixing unint test for prescription_service

--- server/prescription_service/src/test/java/com/prescription_service/prescription_service/PrescriptionServiceApplicationTests.java
+++ server/prescription_service/src/test/java/com/prescription_service/prescription_service/PrescriptionServiceApplicationTests.java
@@ -1,18 +1,13 @@
 package com.prescription_service.prescription_service;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
-class PrescriptionServiceApplicationTests {
+public class PrescriptionServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
