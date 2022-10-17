5182e3fc6f6bc6e3da4c2acec261caadcb066160
angelo <angelocarasig15@gmail.com>
2022-10-17 17:58:18 +1100

    added initial .bat + created medicalhistory service

---
+++ server/medication_service/src/test/java/com/medication_service/medication_service/MedicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medication_service.medication_service;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MedicationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
